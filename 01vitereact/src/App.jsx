c3a1baa57d64367dd9faeb97ddf1d64c0ed2c9f6
Parth Sarthy <parthmishra0219@gmail.com>
2023-12-15 15:26:09 +0530

    createElement in vite react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -6,11 +6,12 @@ import Chai from "./chai"
 
 function App() {
   // const [count, setCount] = useState(0)
-
+const username = "Learning React"
   return (
     <>
     
      <Chai />
+     <h1>Parth Sarthy is {username}</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,9 +3,45 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1> custom App !</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank '
+//   },
+//   children: 'click me to visit google'
+// }
+const anotherUser = "and Alphabet Company";
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com' , target: '_blank'},
+  ' click me to visit google  '  ,
+       anotherUser 
+)
+
+
+
+const anotherElement = (
+  <a href='https://google.com' target= '_blank' rel="noreferrer">visit google</a>
+)
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    </React.StrictMode>,
+  // <React.StrictMode>
+reactElement
+/* <App/> */
+    /* </React.StrictMode>  */
+   
 )
