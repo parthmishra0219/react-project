260d7cb4cf598d21982802137ee03430406ab49f
Parth Sarthy <parthmishra0219@gmail.com>
2023-12-18 11:15:07 +0530

    created github file and fetch data from github

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+function Github() {
+    const [data,setData] = useState([])
+    const [loading, setLoading] = useState(true);
+     useEffect (()=>{
+      fetch("https://api.github.com/users/parthmishra0219")
+      .then(response => response.json())
+      .then(data =>{
+        console.log(data);
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+     },[])
+
+
+
+  return (
+    <div className='text-center text-m-4 m-4 bg-gray-600   text-white p-4 text-3xl'>Github followers : {data.followers} 
+    <img src={data.avatar_url} alt="Git Picture" width={300} />
+    <p className=" text-left mt-4 ml-2">
+            {data.name}
+            <br/>
+            {data.bio} 
+          </p>
+   
+     &nbsp; &nbsp; &nbsp; 
+    </div>
+  )
+}
+
+export default Github
